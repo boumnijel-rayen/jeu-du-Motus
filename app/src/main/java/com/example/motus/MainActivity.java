2c8548fc9a39cfecc35bd7c729fa4daf512e30ca
git@rayen10 <boumnijelrayen9@gmail.com>
2022-12-20 22:10:31 +0100

    UI Home and Start play

--- app/src/main/java/com/example/motus/MainActivity.java
+++ app/src/main/java/com/example/motus/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.motus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startPlay;
+    Button ranking;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startPlay = (Button) findViewById(R.id.start);
+        ranking = (Button) findViewById(R.id.ranking);
+
+        startPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, startPlay.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/motus/startPlay.java
@@ -0,0 +1,14 @@
+package com.example.motus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_play);
+    }
+}
